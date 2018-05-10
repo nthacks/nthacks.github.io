5fc2fd8355b7f65b8d1104d68e17872c9c32e789
d2300b33 <divide2300by33@gmail.com>
2018-05-10 21:54:30 +0530

    Fixes to service worker

--- LinkDrive/sw.js
+++ LinkDrive/sw.js
@@ -1,12 +1,10 @@
 var CACHE='pwabuilder-precache';var precacheFiles=['/','/index.html','/LinkDrive/','/LinkDrive/index.html'];self.addEventListener('install',function(evt)
-{if(evt.request.cache==='only-if-cached'&&evt.request.mode!=='same-origin')
-{return;}
-console.log('The service worker is being installed.');evt.waitUntil(precache().then(function()
-{console.log('[ServiceWorker] Skip waiting on install');return self.skipWaiting();}));});self.addEventListener('activate',function(event)
+{console.log('The service worker is being installed.');evt.waitUntil(precache().then(function()
+{console.log('[ServiceWorker] Skip waiting on install');return self.skipWaiting();}));if(evt.request.cache==='only-if-cached'&&evt.request.mode!=='same-origin')
+{return;}});self.addEventListener('activate',function(event)
 {console.log('[ServiceWorker] Claiming clients for current page');return self.clients.claim();});self.addEventListener('fetch',function(evt)
-{if(evt.request.cache==='only-if-cached'&&evt.request.mode!=='same-origin')
-{return;}
-console.log('The service worker is serving the asset.'+evt.request.url);evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));evt.waitUntil(update(evt.request));});function precache()
+{console.log('The service worker is serving the asset.'+evt.request.url);evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));evt.waitUntil(update(evt.request));if(evt.request.cache==='only-if-cached'&&evt.request.mode!=='same-origin')
+{return;}});function precache()
 {return caches.open(CACHE).then(function(cache)
 {return cache.addAll(precacheFiles);});}
 function fromCache(request)
