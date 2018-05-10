d70e7cba25170f033cf7a7a72a3c7e747c98b474
d2300b33 <divide2300by33@gmail.com>
2018-05-10 21:49:22 +0530

    Fixed try catch block

--- LinkDrive/sw.js
+++ LinkDrive/sw.js
@@ -1,10 +1,10 @@
 var CACHE='pwabuilder-precache';var precacheFiles=['/','/index.html','/LinkDrive/','/LinkDrive/index.html'];self.addEventListener('install',function(evt)
-{if(e.request.cache==='only-if-cached'&&e.request.mode!=='same-origin')
+{if(evt.request.cache==='only-if-cached'&&evt.request.mode!=='same-origin')
 {return;}
 console.log('The service worker is being installed.');evt.waitUntil(precache().then(function()
 {console.log('[ServiceWorker] Skip waiting on install');return self.skipWaiting();}));});self.addEventListener('activate',function(event)
 {console.log('[ServiceWorker] Claiming clients for current page');return self.clients.claim();});self.addEventListener('fetch',function(evt)
-{if(e.request.cache==='only-if-cached'&&e.request.mode!=='same-origin')
+{if(evt.request.cache==='only-if-cached'&&evt.request.mode!=='same-origin')
 {return;}
 console.log('The service worker is serving the asset.'+evt.request.url);evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));evt.waitUntil(update(evt.request));});function precache()
 {return caches.open(CACHE).then(function(cache)
